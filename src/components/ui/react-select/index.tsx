06aa4c8b84c36c67a4606544622e38c5d4b255d5
alifbinhossain <alifahmed47@gmail.com>
2025-05-03 22:35:43 +0600

    style: :art: added react-select-creatable style

--- src/components/ui/react-select/index.tsx
+++ src/components/ui/react-select/index.tsx
@@ -1,8 +1,10 @@
 import { forwardRef } from 'react';
 import { ChevronDown, X } from 'lucide-react';
-import Select, { ClassNamesConfig, components, Props, StylesConfig } from 'react-select';
+import Select, { components, Props, StylesConfig } from 'react-select';
 
 import { cn } from '@/lib/utils';
+
+import { classNames } from './react-select.style';
 
 const selectStyles: StylesConfig = {
 	input: (base) => ({
@@ -17,30 +19,6 @@ const selectStyles: StylesConfig = {
 		};
 	},
 };
-
-const classNames = (extraControlClassName?: string): ClassNamesConfig => ({
-	control: ({ isFocused, isDisabled }) =>
-		cn(
-			'bg-gradient min-h-10 rounded-md border border-input px-3 py-2 text-sm text-foreground',
-			isFocused && 'outline outline-2 outline-offset-2 outline-secondary',
-			isDisabled &&
-				'cursor-not-allowed border-destructive/50 !from-destructive/5 !to-destructive/5 text-destructive',
-			extraControlClassName
-		),
-	placeholder: () => 'text-muted-foreground text-sm',
-	input: () => 'grow',
-	singleValue: () => 'grow',
-	multiValue: () => 'border border-input px-2 rounded bg-base-200',
-	valueContainer: () => 'flex flex-wrap gap-2',
-	menu: () => 'bg-base overflow-hidden  rounded shadow-2xl text-sm border border-input p-1',
-	option: ({ isFocused, isSelected }) =>
-		cn(
-			'rounded px-3 py-1.5 text-sm text-foreground',
-			isFocused && 'bg-base-200',
-			isSelected && 'before:mr-1 before:content-["✔"]'
-		),
-	noOptionsMessage: () => 'text-destructive text-sm p-2 bg-destructive/5 border border-destructive/20 rounded',
-});
 
 export type Ref = any;
 
