0ad31dec754077fa8f100b1b54f1f0f4a8c46348
alifbinhossain <alifahmed47@gmail.com>
2025-05-22 11:46:27 +0600

    ci: :sparkles: added ssr table

--- src/components/core/data-table/_components/export-csv.tsx
+++ src/components/core/data-table/_components/export-csv.tsx
@@ -1,7 +1,6 @@
 import { format } from 'date-fns';
 import { FileSpreadsheet } from 'lucide-react';
 import { CSVLink } from 'react-csv';
-import useTable from '@/hooks/useTable';
 
 import { buttonVariants } from '@/components/ui/button';
 
@@ -9,25 +8,31 @@ import { getFlatHeader } from '@/utils';
 
 import { TTableExportCSV } from '../types';
 
-const TableExportCSV = ({ start_date, end_date }: TTableExportCSV) => {
-	const { table, title, isEntry } = useTable();
-
-	const excludedColumns = new Set(['action', 'actions', 'resetPass']);
-	const csvHeaders: any[] = [];
-	const csvHeadersId: any[] = [];
-
-	table.getAllLeafColumns().forEach(({ id, getIsVisible, columnDef }) => {
-		if (getIsVisible() && !excludedColumns.has(id)) {
-			csvHeaders.push(getFlatHeader(columnDef.header));
-			csvHeadersId.push(id);
-		}
-	});
+const TableExportCSV = ({ start_date, end_date, table, title, isEntry }: TTableExportCSV) => {
 	const filteredRows = table._getFilteredRowModel?.().rows || [];
 
-	// * Generate the CSV data
+	const filteredCsvColumn = table
+		.getAllLeafColumns()
+		.filter(({ getIsVisible, id }) => id !== 'row-selection' && id !== 'actions' && getIsVisible());
+
+	const { csvHeaders, csvHeadersId } = filteredCsvColumn.reduce(
+		(acc, column: any) => {
+			acc.csvHeaders.push(getFlatHeader(column.columnDef.header) as never);
+			acc.csvHeadersId.push(column.id as never);
+			return acc;
+		},
+		{ csvHeaders: [], csvHeadersId: [] }
+	);
+
 	const csvData = [
-		csvHeaders, // * Header row
-		...filteredRows.map((row) => csvHeadersId.map((id) => row.getValue(id))), // * Data rows
+		csvHeaders,
+		...filteredRows.map((row) =>
+			csvHeadersId.map((column) => {
+				if (column === 'created_at') return format(row.original[column], 'dd-MM-yyyy');
+
+				return row.original[column];
+			})
+		),
 	];
 
 	const startTime = format(start_date as Date, 'dd-MM-yyyy');
